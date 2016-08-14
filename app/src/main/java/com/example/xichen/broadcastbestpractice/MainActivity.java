e34c9181502bd09b9b502a82d56c1944c5c00543
pulanyi <chenxi410402@gmail.com>
2016-08-14 22:03:50 +0800

    Local Broadcast

--- app/src/main/java/com/example/xichen/broadcastbestpractice/MainActivity.java
+++ app/src/main/java/com/example/xichen/broadcastbestpractice/MainActivity.java
@@ -1,13 +1,65 @@
 package com.example.xichen.broadcastbestpractice;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    private IntentFilter intentFilter = new IntentFilter();
+    private LocalReceiver localReceiver = new LocalReceiver();
+    private LocalBroadcastManager localBroadcastManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.broadcastbestpractice.LOCAL_BROADCAST");
+                localBroadcastManager.sendBroadcast(intent);
+            }
+        });
+        intentFilter.addAction("com.example.broadcastbestpractice.LOCAL_BROADCAST");
+        localBroadcastManager.registerReceiver(localReceiver, intentFilter);
+        /**
+         * not local
+         *
+         *
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.broadcastbestpractice.MY_BROADCAST");
+                sendOrderedBroadcast(intent, null);
+            }
+        });*/
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        localBroadcastManager.unregisterReceiver(localReceiver);
+    }
+
+    class LocalReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "Received local broadcast", Toast.LENGTH_SHORT).show();
+        }
     }
 }
---
+++ app/src/main/java/com/example/xichen/broadcastbestpractice/MyBroadcastReceiver.java
@@ -0,0 +1,17 @@
+package com.example.xichen.broadcastbestpractice;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by Xi Chen on 2016/8/14.
+ */
+public class MyBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "received in MyBroadcastReceiver", Toast.LENGTH_SHORT).show();
+        abortBroadcast();
+    }
+}
